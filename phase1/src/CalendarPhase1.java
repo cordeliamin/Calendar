12bde5baef6b8bd71c9f7bdee404e6e4cbd997d3
tony_niu <tony.niu@mail.utoronto.ca>
2020-03-03 13:36:21 -0500

    Added JFrame.

--- phase1/src/CalendarPhase1.java
+++ phase1/src/CalendarPhase1.java
@@ -4,9 +4,58 @@ import java.io.IOException;
 import java.sql.SQLOutput;
 import java.util.HashMap;
 import java.util.Scanner;
+import javax.swing.*;
 import javax.swing.JOptionPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class CalendarPhase1 {
+
+    public static class accountButton{
+
+        accountButton(){
+            JFrame f = new JFrame("Account");
+            JButton b1 = new JButton("Yes");
+            JButton b2 = new JButton("No");
+
+            b1.setBounds(70,100,90, 30);
+            b2.setBounds(170, 100, 90, 30);
+
+            JLabel label = new JLabel();
+            label.setText("Do you have an existing account?");
+            label.setBounds(10, 10, 300, 100);
+
+            f.add(label);
+            f.add(b1);
+            f.add(b2);
+            f.setSize(300,300);
+            f.setLayout(null);
+            f.setVisible(true);
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            //action listener
+            b1.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent arg0) {
+                    System.out.println("yes!");
+                    System.exit(0);
+                }
+            });
+            b2.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    System.out.println(("No!"));
+                    System.exit(0);
+                }
+            });
+        }
+    }
+
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
@@ -17,15 +66,15 @@ public class CalendarPhase1 {
         /*
         Creating a map of all users in the csv file
          */
+        new accountButton();
+
         HashMap<String, String> users = getUsers();
+
 
         /*
         boolean accountExist;
         accountExist = JOptionPane.showInputDialog("Do you have an account?");
         */
-
-
-
 
         /*
         Prompt user to log in
