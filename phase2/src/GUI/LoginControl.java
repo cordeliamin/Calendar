f79e1faf4e308a0809a7ce5249c0c364a0c9aaab
BlueRogue7714 <37222075+BlueRogue7714@users.noreply.github.com>
2020-04-13 16:58:00 -0400

    Added additional error handling to user creation

--- phase2/src/GUI/LoginControl.java
+++ phase2/src/GUI/LoginControl.java
@@ -2,6 +2,7 @@ package GUI;
 
 import CalendarSystem.Calendar;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -11,6 +12,7 @@ import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -34,7 +36,9 @@ public class LoginControl extends Controller {
 
     @FXML
     private void newUser() throws IOException {
-        setScreen("UserCreatorScene.fxml", createNewUser);
+        FXMLLoader signUpLoader = setScreenAndGetLoader("UserCreatorScene.fxml", createNewUser);
+        UserCreatorControl signUpControl = signUpLoader.getController();
+        signUpControl.setUsernames(new ArrayList<>(getUsers().keySet()));
     }
 
     @FXML
